29cfde650c2854770872a8001e928193ecfe02eb
Khaiam <khaiam@mail.ru>
2024-05-01 22:27:34 +0300

    admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import TelegramUser, Transaction, Catalog, Product, TelegramAccount, Order, ProductContent
 
-# Register your models here.
+
+class ProductContentAdminInline(admin.TabularInline):
+    model = ProductContent
+
+    def get_extra(self, request, obj=None, **kwargs):
+        extra = 1
+        return extra
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_filter = ('is_confirmed',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = (ProductContentAdminInline,)
+
+
+@admin.register(TelegramAccount)
+class TelegramAccountAdmin(admin.ModelAdmin):
+    list_filter = ('is_banned',)
+
+
+admin.site.register(TelegramUser)
+admin.site.register(Catalog)
+admin.site.register(Order)
