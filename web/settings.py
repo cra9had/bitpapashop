1abf4af18e2f61a6e266047fd0e1a1de6ed0cf00
Khaiam <khaiam@mail.ru>
2024-05-01 23:53:11 +0300

    django_rq

--- web/settings.py
+++ web/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'main'
+    'main',
+
+    'django_rq',
+
 ]
 
 MIDDLEWARE = [
@@ -100,6 +103,47 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+RQ_QUEUES = {
+    'default': {
+        'HOST': 'localhost',
+        'PORT': 6379,
+        'DB': 0,
+        'USERNAME': '',
+        'PASSWORD': '',
+        'DEFAULT_TIMEOUT': 360,
+        'REDIS_CLIENT_KWARGS': {    # Eventual additional Redis connection arguments
+            'ssl_cert_reqs': None,
+        },
+    },
+    'with-sentinel': {
+        'SENTINELS': [('localhost', 26736), ('localhost', 26737)],
+        'MASTER_NAME': 'redismaster',
+        'DB': 0,
+        # Redis username/password
+        'USERNAME': 'redis-user',
+        'PASSWORD': 'secret',
+        'SOCKET_TIMEOUT': 0.3,
+        'CONNECTION_KWARGS': {  # Eventual additional Redis connection arguments
+            'ssl': True
+        },
+        'SENTINEL_KWARGS': {    # Eventual Sentinel connection arguments
+            # If Sentinel also has auth, username/password can be passed here
+            'username': 'sentinel-user',
+            'password': 'secret',
+        },
+    },
+    'high': {
+        'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379/0'), # If you're on Heroku
+        'DEFAULT_TIMEOUT': 500,
+    },
+    'low': {
+        'HOST': 'localhost',
+        'PORT': 6379,
+        'DB': 0,
+    }
+}
 
 
 # Internationalization
